f59b3695e4150910404e2f747124cfc5c947e9f3
Ryan Blau <ryan.blau@wework.com>
2018-02-10 17:45:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board
   print "   |   |   "
-  print "-----------"
-  print "   |   |   "
-  print "-----------"
+  "-----------"
+  "   |   |   "
+  "-----------"
   "   |   |   "
   end
   
